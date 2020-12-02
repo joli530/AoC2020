43081a8e71eedbcc6487463f92d725a3437d7df7
Johan <joli530@live.com>
2020-12-02 17:17:54 +0100

    Day2 start

---
+++ AdventOfCode/2020/SolverDay2.cs
@@ -0,0 +1,32 @@
+using AoC.Common;
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace AoC.Solvers
+{
+    public class SolverDay2 : Solver
+    {
+        List<int> input = new List<int>();
+        public SolverDay2(string inputFile):base(inputFile)
+        {
+            
+
+        } 
+        public override int SolvePart1()
+        {
+           
+            throw new System.Exception("Solution not found");
+        }
+        public override int SolvePart2()
+        {
+            throw new System.Exception("Solution not found");
+        }
+    }
+       class password{
+        public password(string inputRow)
+        {
+            throw new System.Exception("Solution not found");
+        }
+    }
+}
